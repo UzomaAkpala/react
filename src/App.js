7ae3cd3e3c7bcc6124fb7fee0cdba06b184eae7a
UzomaAkpala <francisakpala@yahoo.com>
2023-05-17 21:20:08 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,47 +1,106 @@
 import Employee from './components/Employee';
-import './App.css';
+import './index.css'
 import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddEmployee from './components/AddEmployee';
+import EditEmployee from './components/EditEmployee';
 
 export default function App() {
- const [role, setRole] = useState('Senior Dev')
   const showEmployees = true
-  return (
-    <div className="App">
-      <header className="App-header">
-        {showEmployees ? 
-        <>
+  const [employees, setEmployees] = useState([
 
-        <input
-         type='text' 
-         placeholder='role'
-         onChange={(e) => {
-          setRole(e.target.value)
-        }} />
-          <Employee
-           name = "Uzoma Akpala"
-           role = {role} 
-           />
-          <Employee
-           name ="Jovita Akpala"
-            role = "Banker and Youtuber"
-             />
-          <Employee
-           name = "Jane Akpala"
-            role = "Senior Student"
-             />
-          <Employee
-           name = "Joshua Akpala"
-            role = "Junior Student"
-             />
-          <Employee
-           name = "Julia Akpala"
-            role = "Nusery Baby"
-            />
-        </>
+    {
+      id: 1,
+    name: "Uzoma Akpala",
+    role:  "Senior Dev",
+     img:"https://images.pexels.com/photos/3748221/pexels-photo-3748221.jpeg"
+      },
+      {
+        id:2,
+      name:"Jovita Akpala",
+      role: "Banker and Youtuber",
+      img: 'https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg'
+      },
+      {
+        id:3,
+        name: "Jane Akpala",
+        role: "Senior Student",
+        img: 'https://images.pexels.com/photos/4355346/pexels-photo-4355346.jpeg' 
+      },
+      {
+        id:4,
+        name: "Joshua Akpala",
+           role: "Junior Student",
+           img: 'https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg'
+      },
+      {
+        id:5,
+        name: "Julia Akpala",
+        role: "Nusery baby",
+        img: 'https://images.pexels.com/photos/3220360/pexels-photo-3220360.jpeg'
+      },
+      {
+        id:6,
+        name: "Machi Akpala",
+            role: "Business Man",
+            img: 'https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg'
+      }
+
+  ])
+     
+      function updateEmployee(id,newName, newRole){
+        const updatedEmployees = employees.map((employee) => {
+          if(id===employee.id){
+            return{...employee, name:newName, role: newRole}
+          }
+          return employee
+        })
+        setEmployees(updatedEmployees)
+      }
+
+        function newEmployee(name, role, img){
+          const newEmployee = {
+            id: uuidv4(),
+            name: name,
+            role: role,
+            img: img
+          }
+          setEmployees([...employees, newEmployee])
+        }
+
+  return (
+    <div className='App'>
+        {showEmployees ? 
+        <div className='flex flex-wrap justify-center'>
+        {employees.map((employee) => {
+        const editEmployee = ( <EditEmployee
+        name ={employee.name} 
+        role = {employee.role}
+        updateEmployee = {updateEmployee}
+        id = {employee.id} 
+        />
+        )
+
+        
+
+        return(  
+        <Employee
+           key = {employee.id}
+           id = {employee.id}
+          name = {employee.name}
+          role = {employee.role}
+          img = {employee.img}
+          EditEmployee = {editEmployee}
+          />
+          )
+        })}
+          <AddEmployee newEmployee = {newEmployee} />
+        </div>
+      
         :
         <p>You cannot see the employeees</p>
         }
-      </header>
     </div>
   );
 }
