d5487c77863511c9b66a0fbf0ef4388250e62fce
UzomaAkpala <francisakpala@yahoo.com>
2023-05-17 21:50:31 +0100

    more updates

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {v4 as uuidv4} from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddEmployee from './components/AddEmployee';
 import EditEmployee from './components/EditEmployee';
+import Header from './components/Header';
 
 export default function App() {
   const showEmployees = true
@@ -70,9 +71,10 @@ export default function App() {
         }
 
   return (
-    <div className='App'>
+    <div className='App bg-gray-300 min-h-screen'>
+      <Header />
         {showEmployees ? 
-        <div className='flex flex-wrap justify-center'>
+        <div className='flex flex-wrap justify-center my-2'>
         {employees.map((employee) => {
         const editEmployee = ( <EditEmployee
         name ={employee.name} 
